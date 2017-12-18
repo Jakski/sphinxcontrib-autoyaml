f2cf052a1fd7a23b93e8bff1db2d83db6917147b
Jakub Pieńkowski <jpienkowski@centuria.pl>
2017-12-18 12:07:15 +0100

    Change fix version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     license='MIT',
     description='Sphinx extension to generate docs from YAML comments',
     platforms='any',
-    version='0.3.0',
+    version='0.3.1',
     packages=['sphinxcontrib.autoyaml'],
     classifiers=[
         'Development Status :: 3 - Alpha',
