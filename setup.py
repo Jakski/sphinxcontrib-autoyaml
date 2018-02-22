24db2e640e75bb5a85e54f9bd49c8f2c70c2c7cd
Jakski <8525083+Jakski@users.noreply.github.com>
2018-02-22 19:57:58 +0100

    Fix typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 def get_latest_version(changelog):
     """Retrieve latest version of package from changelog file."""
-    # Match strings like "### [1.2.3] - 2017-02-02"
+    # Match strings like "## [1.2.3] - 2017-02-02"
     regex = r"^##\s*\[(\d+.\d+.\d+)\]\s*-\s*\d{4}-\d{2}-\d{2}$"
     with open(changelog, "r") as changelog:
         content = changelog.read()
